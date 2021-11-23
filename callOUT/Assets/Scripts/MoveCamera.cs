1072cf624d8b707e57232ee9af2b836f978d9ce7
colorplease <alistairvtran@gmail.com>
2021-11-22 18:18:59 -0800

    new camera

--- callOUT/Assets/Scripts/MoveCamera.cs
+++ callOUT/Assets/Scripts/MoveCamera.cs
@@ -2,9 +2,48 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
 
-    public Transform player;
+     [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
 
-    void Update() {
-        transform.position = player.transform.position;
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    [SerializeField] float mouseMultiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
+    {
+        cam = GameObject.Find("eyes").GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        MyInput();
+        ControlCamera();
+    }
+
+    void MyInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * mouseMultiplier * 2;
+        xRotation -= mouseY * sensY * mouseMultiplier * 2;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+    }
+
+    void ControlCamera()
+    {
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
