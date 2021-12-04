c7d3ba2bad9cadb9d3e71ebcf359daa71ee0cc0e
colorplease <alistairvtran@gmail.com>
2021-12-04 10:20:56 -0800

    bullet interaction work

--- callOUT/Assets/Scripts/Bullet.cs
+++ callOUT/Assets/Scripts/Bullet.cs
@@ -8,13 +8,22 @@ public class Bullet : MonoBehaviour
     [SerializeField]GameObject impactParticle;
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag != "Player")
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "Kazoo")
         {
         lastBulletImpact = transform.position;
         impactParticle.GetComponent<ParticleSystemRenderer>().material = other.gameObject.GetComponent<Renderer>().material; 
         GameObject deez = Instantiate(impactParticle, lastBulletImpact, transform.rotation);
         deez.transform.Rotate(0f, 180f, 0f);
         Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "Kazoo")
+        {
+            other.gameObject.GetComponent<KazooManAI>().health -= 10;
+            Destroy(gameObject);
         }
     }
 
--- callOUT/Assets/Scripts/KazooManAI.cs
+++ callOUT/Assets/Scripts/KazooManAI.cs
@@ -7,6 +7,8 @@ public class KazooManAI : MonoBehaviour
 {
     [SerializeField]NavMeshAgent navMeshAgent;
     [SerializeField]Transform target;
+    [SerializeField]GameObject ragDoll;
+    public float health;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,27 @@ public class KazooManAI : MonoBehaviour
     void Update()
     {
         navMeshAgent.destination = target.position;
+
+        if (health <= 0)
+        {
+            Instantiate(ragDoll, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Slope")
+        {
+            navMeshAgent.speed = 10;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Slope")
+        {
+            navMeshAgent.speed = 2.5f;
+        }
     }
 }
