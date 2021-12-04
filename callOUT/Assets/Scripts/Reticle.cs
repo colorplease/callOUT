786f30a9833185616cd1cd45d7b2d19e24604562
colorplease <alistairvtran@gmail.com>
2021-12-03 16:55:56 -0800

    crosshair done

---
+++ callOUT/Assets/Scripts/Reticle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle : MonoBehaviour
+{
+     [SerializeField] RectTransform reticle;
+     [SerializeField] Rigidbody player;
+     public float restingSize;
+     public float maxSize;
+     public float speedReturnMultiplier;
+     public float speedReturn;
+     float currentSize;
+    // Start is called before the first frame update
+    void Update()
+    {
+       if (isMoving)
+       {
+           currentSize = Mathf.Lerp(currentSize, maxSize, Time.deltaTime * player.velocity.sqrMagnitude * speedReturnMultiplier);
+       }
+       else
+           {
+               currentSize = Mathf.Lerp(currentSize, restingSize, Time.deltaTime * speedReturn);
+           }
+        reticle.sizeDelta = new Vector2(currentSize, currentSize);
+    }
+
+    bool isMoving
+    {
+        get
+       {
+           if(player.velocity.sqrMagnitude > 0.1f
+           )
+            return true;
+            else
+            return false;
+       }
+    }
+
+}
